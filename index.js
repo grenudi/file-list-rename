9a0ef32a8ad0d15270afefa96d8d4398bd70048a
grenudi <idunerg@gmail.com>
2018-07-01 05:18:56 +0300

    nah it's done 3

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const patterns = require("./patterns");
 
 const fuckyoujavascript =new constructor("./inandout");
 fuckyoujavascript.run(patterns.numToStr,"Empire_V.mp3");
+//// TODO: make constructor to accept a list of patterns through parameters 
